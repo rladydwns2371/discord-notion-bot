a3c7b6ead80c34665eb3d5ccb66d54c143180658
peach <144201031+rladydwns2371@users.noreply.github.com>
2026-07-09 15:41:20 +0000

    도메인별 담당자 매핑 추가

---
+++ src/domainOwners.js
@@ -0,0 +1,14 @@
+const DOMAIN_OWNERS = {
+  admin: '김용준',
+  recommendation: '김용준',
+  auth: '김동현',
+  user: '김동현',
+  chatbot: '김동훈',
+  course: '이강욱',
+  enrollment: '이강욱',
+  badge: '배정현',
+  problems: '배정현',
+  ranking: '배정현',
+};
+
+module.exports = { DOMAIN_OWNERS };
